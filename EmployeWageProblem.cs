4e60eaf2bc83433b846794ddfe99a1ed7c428692
Anisha0906 <anishadas880@gmail.com>
2022-08-05 20:46:29 +0530

    Add:Calculate Wages till

--- EmployeWageProblem.cs
+++ EmployeWageProblem.cs
@@ -10,15 +10,18 @@ namespace EmployeeWageProblemUsingOops
     {
         const int Emp_Full_Time = 1;
         const int Emp_Part_Time = 2;
-        public int Working_Days_Per_Month = 20;
-        public int Total_Salary_Month = 0;
-        public int Day;
+        public int Count_Work_Day = 20;
+        public int Emp_Per_Rate_Hour = 20;
+        public int MAx_Hrs_Or_Day_Month = 100;
+        public int Working_Days = 0;
         public int empHrs = 0;
-        public int EmployeeWage;
+        public int EmployeeWage = 0;
+        public int EmpSalary;
         public void Attendance()
         {
-            for (Day = 1; Day <= Working_Days_Per_Month; Day++)
+            while (empHrs <= MAx_Hrs_Or_Day_Month && Working_Days < Count_Work_Day)  
             {
+                Working_Days++; 
                 Random random = new Random(); 
                 int empCheck = random.Next(0, 3); 
                 switch (empCheck) 
@@ -35,9 +38,10 @@ namespace EmployeeWageProblemUsingOops
                         empHrs = 0; 
                         break;
                 }
-            } 
-            EmployeeWage = empHrs * Working_Days_Per_Month;
-            Total_Salary_Month += EmployeeWage;  
+                EmployeeWage += empHrs; 
+                Console.WriteLine("Day#:" + Working_Days + "Employee Hrs:" + empHrs);
+            }
+            EmpSalary = EmployeeWage + Emp_Per_Rate_Hour; 
             Console.WriteLine("Employee Wage : " + EmployeeWage); 
         }
     }
