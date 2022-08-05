bcef023712546ca6062711ef140e69f6391f5de7
Anisha0906 <anishadas880@gmail.com>
2022-08-05 20:39:45 +0530

    Add:Calculate Wages For A Month

--- EmployeWageProblem.cs
+++ EmployeWageProblem.cs
@@ -10,30 +10,35 @@ namespace EmployeeWageProblemUsingOops
     {
         const int Emp_Full_Time = 1;
         const int Emp_Part_Time = 2;
-        public int Wage_Per_Hours = 20;
+        public int Working_Days_Per_Month = 20;
+        public int Total_Salary_Month = 0;
+        public int Day;
         public int empHrs = 0;
         public int EmployeeWage;
         public void Attendance()
         {
-          Random random = new Random();
-          int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            for (Day = 1; Day <= Working_Days_Per_Month; Day++)
             {
-                case Emp_Full_Time:
-                    empHrs = 8; 
-                 break;
+                Random random = new Random(); 
+                int empCheck = random.Next(0, 3); 
+                switch (empCheck) 
+                {
+                    case Emp_Full_Time:
+                        empHrs = 8; 
+                        break;
 
-                case Emp_Part_Time: 
-                    empHrs = 4; 
-                    break;
+                    case Emp_Part_Time: 
+                        empHrs = 4; 
+                        break;
 
-                default:
-                    empHrs = 0; 
-                    break;
-            }
-
-            EmployeeWage = empHrs * Wage_Per_Hours;
-            Console.WriteLine("Employee wage: " + EmployeeWage); 
+                    default:
+                        empHrs = 0; 
+                        break;
+                }
+            } 
+            EmployeeWage = empHrs * Working_Days_Per_Month;
+            Total_Salary_Month += EmployeeWage;  
+            Console.WriteLine("Employee Wage : " + EmployeeWage); 
         }
     }
 }
