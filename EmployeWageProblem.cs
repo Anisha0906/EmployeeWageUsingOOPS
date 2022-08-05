8d4b7e23be7ea0a6f8ee66ec6b8cfcada72ee151
Anisha0906 <anishadas880@gmail.com>
2022-08-05 20:23:07 +0530

    Add:Part time Employee Wage

--- EmployeWageProblem.cs
+++ EmployeWageProblem.cs
@@ -10,28 +10,31 @@ namespace EmployeeWageProblemUsingOops
     {
         public int IS_PRESENT = 1;
         public int Wage_Per_Hours = 20;
-        public int Full_Day_Hour = 8;
+         readonly int Emp_Full_Time = 2;
+        readonly int Emp_Part_Time = 1;
+        public int empHrs;
         public int EmployeeWage;
         public void Attendance()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == IS_PRESENT)
+            if (empCheck == Emp_Full_Time)
             {
                 Console.WriteLine("Employee is Present");
-                CalculateDailyWage();
+                empHrs = 8;
+            }
+            else if (empCheck ==Emp_Part_Time)
+            {
+                Console.WriteLine("Employee is Part Time");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
-                Console.WriteLine("Employee Wage is null");
+                empHrs = 0;
             }
-
-        }
-        public void CalculateDailyWage()
-        {
-            EmployeeWage = Wage_Per_Hours * Full_Day_Hour;
-            Console.WriteLine("Employee Full Wage is : " + EmployeeWage);
+            EmployeeWage = empHrs * Wage_Per_Hours;
+            Console.WriteLine("Employee Full Wage is : " + EmployeeWage); 
         }
     }
 }
