d0655ecb691a23bf6a5b17dd1b796302bcbfc805
Anisha0906 <anishadas880@gmail.com>
2022-08-07 23:30:07 +0530

    Add:Ability to manage Employee Wage For Multiple Companies

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemUsingOops
+{   
+    interface IComputeEmpWage
+    {
+        void AddCompany(string companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours);
+        int ComputeMonthlyWage(CompanyEmpWage company);
+        void DisplayCompanyWages();
+    }
+}
